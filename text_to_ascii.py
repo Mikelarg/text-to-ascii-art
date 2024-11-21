42064b9c6c62ce340553ab5c1f82d1db50c1a91f
Miller-du <official.shutu@gmail.com>
2024-11-21 13:22:31 +0800

    uniform code style

--- text_to_ascii.py
+++ text_to_ascii.py
@@ -1,12 +1,13 @@
 # coding=utf-8
-"""
+
+'''
 Installation:
-    Option 1: Install Pillow directly:
+    Install Pillow directly:
         pip install Pillow
-    Option 2: Install all dependencies from requirements.txt:
+    or install all dependencies from requirements.txt:
         pip install -r requirements.txt
 All done!
-"""
+'''
 
 from PIL import Image, ImageFont, ImageDraw
 
@@ -29,7 +30,7 @@ import argparse
     Arguments Parser Init
 '''
 
-parser = argparse.ArgumentParser(description="Text to ASCII")
+parser = argparse.ArgumentParser(description='Text to ASCII')
 parser.add_argument('-text', help='Your text (Required)', type=str, required=True)
 parser.add_argument('-char', help='Your char for ASCII (Optional, default char — █)', default='█', type=str,
                     required=False)
@@ -39,30 +40,30 @@ parser.add_argument('-space-char', help=r'Your space char (Optional, default spa
 parser.add_argument('-gap-char', help='Your char between letters (Optional, default space char — empty)', default='',
                     type=str,
                     required=False)
-parser.add_argument('-font', help="Your Font (Optional, default font — Arial)", type=str, default='Arial.ttf',
+parser.add_argument('-font', help='Your Font (Optional, default font — Arial)', type=str, default='Arial.ttf',
                     required=False)
-parser.add_argument('-align', help="Text Align(center, left, right) (Optional, default align — left)", type=str,
-                    default="left", required=False)
-parser.add_argument('-spacing', help="Text Spacing (Optional, default spacing — 0)", type=int,
+parser.add_argument('-align', help='Text Align(center, left, right) (Optional, default align — left)', type=str,
+                    default='left', required=False)
+parser.add_argument('-spacing', help='Text Spacing (Optional, default spacing — 0)', type=int,
                     default=0, required=False)
-parser.add_argument('-size', help="Font Size (Optional, default size — 10)", type=int, default=10, required=False)
-parser.add_argument('-signature', help="Your Signature. \\n — for new line(Optional, default signature — mine(✿´‿`))",
+parser.add_argument('-size', help='Font Size (Optional, default size — 10)', type=int, default=10, required=False)
+parser.add_argument('-signature', help='Your Signature. \\n — for new line(Optional, default signature — mine(✿´‿`))',
                     type=str,
-                    default="Made by Mikelarg\\nhttps://vk.com/mikelarg",
+                    default='Made by Mikelarg\\nhttps://vk.com/mikelarg',
                     required=False)
-parser.add_argument('--left', action='store_true', help="If argument is defined, script writes you signature left side",
+parser.add_argument('--left', action='store_true', help='If argument is defined, script writes you signature left side',
                     required=False)
-parser.add_argument('--hr', action='store_true', help="If argument is defined, script writes hr", required=False)
-parser.add_argument('-hr-char', help="Your HR char", type=str, default='_',
+parser.add_argument('--hr', action='store_true', help='If argument is defined, script writes hr', required=False)
+parser.add_argument('-hr-char', help='Your HR char', type=str, default='_',
                     required=False)
-parser.add_argument('--save-image', action='store_true', help="Save text image to PNG", required=False)
+parser.add_argument('--save-image', action='store_true', help='Save text image to PNG', required=False)
 
 '''
     Get and Filter Arguments Values
 '''
 
 args = parser.parse_args()
-args.text = args.text.replace("\\n", "\n")
+args.text = args.text.replace('\\n', '\n')
 args.align = args.align.lower()
 
 font = ImageFont.truetype(args.font, args.size)
@@ -72,7 +73,7 @@ font = ImageFont.truetype(args.font, args.size)
 '''
 
 size = [0, 0]
-text_lines = args.text.split("\n")
+text_lines = args.text.split('\n')
 for line in text_lines:
     bbox = font.getbbox(line)
     line_width = bbox[2] - bbox[0]
@@ -121,7 +122,7 @@ if args.hr:
 '''
 
 signature = args.signature
-lines = signature.split("\\n")
+lines = signature.split('\\n')
 for line in lines:
     if args.left:
         print(line)
